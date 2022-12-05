c763bcf2e90ec04fabd7de6102381de3bd4c52d0
bappyasif <bappyasif@hotmail.com>
2022-12-05 19:25:41 +0600

    added some features in dropdown options
    * upon clicking on any option will result dropdown to close
    * added a custom hook to make dropdown to close when its been clicked from anywhere but that container div

--- client-side/src/components/MainNavigation.js
+++ client-side/src/components/MainNavigation.js
@@ -1,13 +1,13 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { LoginTwoTone, AppRegistrationTwoTone, TimelineTwoTone, VerifiedUserSharp, DynamicFeedSharp, PeopleTwoTone, PersonTwoTone } from "@mui/icons-material";
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import { LoginTwoTone, AppRegistrationTwoTone, VerifiedUserSharp, DynamicFeedSharp, PeopleTwoTone, PersonTwoTone } from "@mui/icons-material";
 import { H1Element, H4Element, NavElement, WrapperDiv } from './GeneralElements'
 import { MuiButtonElement, MuiInputElement, TabElement } from './MuiElements';
-import { FormElement, FormElementForwardedRef } from './FormElements';
 import { logoutUserFromApp, sendDataToServer } from './utils';
 import { AppContexts } from '../App';
 import { useNavigate } from 'react-router-dom';
-import { Avatar, Button, Paper, Tooltip, Typography } from '@mui/material';
+import { Avatar, Button, Tooltip, Typography } from '@mui/material';
 import { Stack } from '@mui/system';
+import { useToCloseModalOnClickedOutside } from './hooks/toDetectClickOutside';
 
 function MainNavigation() {
   let appCtx = useContext(AppContexts);
@@ -35,30 +35,44 @@ function MainNavigation() {
 
 const FloatingAuthenticatedUserFunctionality = ({ appCtx }) => {
   let [showDropdown, setShowDropdown] = useState(false)
+  
   let toggleDropdown = () => setShowDropdown(!showDropdown)
+  
   let closeDropdown = () => setShowDropdown(false);
+  
   return (
     <Stack sx={{ flexDirection: "row", gap: 4, position: "relative", alignItems: "center" }}>
       <Typography variant="h6">Welcome, Dear {appCtx.user.fullName}</Typography>
       <Avatar onClick={toggleDropdown} alt={`profile picture of ${appCtx.user.fullName}`} src={appCtx.user.ppUrl || "https://random.imagecdn.app/500/150"} />
-      {showDropdown ? <ShowAuthUserDropdowns /> : null}
+      {showDropdown ? <ShowAuthUserDropdowns closeDropdown={closeDropdown} /> : null}
     </Stack>
   )
 }
 
-let ShowAuthUserDropdowns = () => {
+let ShowAuthUserDropdowns = ({ closeDropdown }) => {
   let options = [{ name: "Edit Profile", icon: null }, { name: "Logout", icon: null }]
-  let renderOptions = () => options.map(item => <RenderDropDownOption key={item.name} item={item} />)
+
+  let renderOptions = () => options.map(item => <RenderDropDownOption key={item.name} item={item} closeDropdown={closeDropdown} />)
+  
+  let ref = useRef();
+  
+  useToCloseModalOnClickedOutside(ref, closeDropdown)
 
   return (
-    <Stack sx={{ position: "absolute", right: 0, top: "62px", gap: "9px", backgroundColor: "gainsboro", p: 2 }}>
+    <Stack
+      ref={ref} 
+      sx={{
+        position: "absolute", right: 0, top: "62px", 
+        gap: "9px", backgroundColor: "gainsboro", p: 2
+      }}
+    >
       {renderOptions()}
     </Stack>
   )
 
 }
 
-const RenderDropDownOption = ({ item }) => {
+const RenderDropDownOption = ({ item, closeDropdown }) => {
   let appCtx = useContext(AppContexts);
 
   const navigate = useNavigate();
@@ -75,15 +89,17 @@ const RenderDropDownOption = ({ item }) => {
 
   let handleClick = () => {
     if (item.name === "Logout") {
+      closeDropdown()
       handleLogoutUser()
     } else if (item.name === "Edit Profile") {
-      console.log("eduit proifle!!")
+      closeDropdown()
+      navigate(`/edit-user-profile`);
     }
   }
 
   return (
-    <Tooltip sx={{mb: .2}} title={item.name}>
-      <Button onClick={handleClick} startIcon={item.icon} sx={{outline: "solid 2px red"}}>
+    <Tooltip sx={{ mb: .2 }} title={item.name}>
+      <Button onClick={handleClick} startIcon={item.icon} sx={{ outline: "solid 2px red" }}>
         <Typography variant='subtitle2'>{item.name}</Typography>
       </Button>
     </Tooltip>
@@ -99,7 +115,6 @@ export let NavigationLinks = () => {
       <TabElement className={"nav-item"} labelText={"Register"} path={"register"} icon={<AppRegistrationTwoTone />} />
       <TabElement className={"nav-item"} labelText={"Connect"} path={"connect"} icon={<VerifiedUserSharp />} />
       <TabElement className={"nav-item"} labelText={"Feeds"} path={"news-feeds"} icon={<DynamicFeedSharp />} />
-      {/* <TabElement className={"nav-item"} labelText={"Friends"} path={"friend-requests"} icon={<PeopleTwoTone />} /> */}
       <TabElement className={"nav-item"} labelText={"Friends"} path={"user-friendships"} icon={<PeopleTwoTone />} />
       <TabElement className={"nav-item"} labelText={"Profile"} path={`users/${appCtx.user?._id}/profile`} icon={<PersonTwoTone />} />
     </NavElement>
@@ -118,6 +133,7 @@ let NavsWhenUserIsNotAuthenticated = () => {
 let NavsWhenUserAuthenticated = ({ appCtx }) => {
   return (
     <NavElement className="main-nav">
+      <TabElement className={"nav-item"} labelText={"Feeds"} path={"/"} icon={<DynamicFeedSharp />} />
       <TabElement className={"nav-item"} labelText={"Connect"} path={"connect"} icon={<VerifiedUserSharp />} />
       <TabElement className={"nav-item"} labelText={"Friends"} path={"user-friendships"} icon={<PeopleTwoTone />} />
       <TabElement className={"nav-item"} labelText={"Profile"} path={`users/${appCtx.user?._id}/profile`} icon={<PersonTwoTone />} />
@@ -139,7 +155,6 @@ let FloatingLogin = () => {
 
   let updateData = result => {
     appCtx.handleData(result)
-    // console.log(result, "result!!")
     ref.current?.reset();
     result.user?.topics.length < 4 ? navigate("/choose-topics") : navigate("/");
   }
@@ -154,8 +169,6 @@ let FloatingLogin = () => {
   return (
     <WrapperDiv className="fl-wrapper">
       <H4Element value={"Login to your profile"} />
-      {/* <FormElement handleSubmit={handleSubmit} > */}
-      {/* <FormElementForwardedRef handleSubmit={handleSubmit} ref={ref}> */}
       <form ref={ref} method={"post"} onSubmit={handleSubmit}>
         <MuiInputElement
           type={"email"}
@@ -177,8 +190,6 @@ let FloatingLogin = () => {
         />
         <MuiButtonElement type={"submit"} text="Login" />
       </form>
-      {/* </FormElementForwardedRef> */}
-      {/* </FormElement> */}
     </WrapperDiv>
   )
 }
