b5474a471003a4b42f78459f5f5a295aa8496220
bappyasif <bappyasif@hotmail.com>
2022-12-09 21:58:36 +0600

    added feature
    * from home page or feeds page posters profile page can be viewed as well
    * profile page is can only be viewed if you current user is authenticated or logged in
    * used pure css based tooltips notification for this segement instead of mui tooltips component
    * refactored RenderPostData into a mui card component

--- client-side/src/components/RenderPostData.js
+++ client-side/src/components/RenderPostData.js
@@ -1,6 +1,8 @@
-import { Typography } from '@mui/material'
+import { MoreVertTwoTone } from '@mui/icons-material'
+import { Avatar, Card, CardContent, CardHeader, IconButton, Tooltip, Typography } from '@mui/material'
 import moment from 'moment'
 import React, { useContext, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import { AppContexts } from '../App'
 import { CardHeaderElement } from './MuiElements'
 import ShowUserPostMedias from './ShowUserPostMedias'
@@ -35,22 +37,74 @@ function RenderPostDataEssentials({ postData, shareMode }) {
 
     return (
         <>
-            <CardHeaderElement
-                avatarUrl={userData?.ppUrl || "https://random.imagecdn.app/500/150"}
-                altText={"fullname"}
-                // title={appCtx.user?.fullName || "User Name"}
-                // joined={appCtx.user?.created || Date.now()}
-                title={userData?.fullName || "User Name"}
-                joined={userData?.created || Date.now()}
-                forPost={true}
-            />
+            {/* <Link id='posted-by' style={{ textDecoration: "none" }} to={`/users/${userData._id}/visit/profile`}> */}
+            {/* <p className='tooltip-text'>Visit Profile</p> */}
+            {/* <CardHeaderElement
+                    avatarUrl={userData?.ppUrl || "https://random.imagecdn.app/500/150"}
+                    altText={"fullname"}
+                    title={userData?.fullName || "User Name"}
+                    joined={userData?.created || Date.now()}
+                    forPost={true}
+                /> */}
+            {/* </Link> */}
 
-            {shareMode ? null : <Typography sx={{ color: "text.secondary", position: "absolute", top: 29, right: 20 }} variant="subtitle2">{`Live Since: ${moment(created).fromNow()}`}</Typography>}
+            {/* <p className='posted-by-tooltip-text'>Visit Profile</p> */}
 
-            <Typography variant='h4' sx={{ backgroundColor: "honeydew", p: .2, mr: 6, ml: 15 }} dangerouslySetInnerHTML={{ __html: body }}></Typography>
+            {/* <Typography variant='h4' sx={{ backgroundColor: "honeydew", p: .2, mr: 6, ml: 15 }} dangerouslySetInnerHTML={{ __html: body }}></Typography> */}
 
-            {preparingAdditionalsForRendering.Id ? <ShowUserPostMedias mediaContents={preparingAdditionalsForRendering} /> : null}
+            {/* {preparingAdditionalsForRendering.Id ? <ShowUserPostMedias mediaContents={preparingAdditionalsForRendering} /> : null} */}
+
+            <Card>
+                <RenderPostCardHeader userData={userData} />
+
+                {shareMode ? null : <Typography sx={{ color: "text.secondary", position: "absolute", top: 29, right: 20 }} variant="subtitle2">{`Live Since: ${moment(created).fromNow()}`}</Typography>}
+
+                <RenderPostCardContent body={body} preparingAdditionalsForRendering={preparingAdditionalsForRendering} />
+            </Card>
         </>
+    )
+}
+
+const RenderPostCardContent = ({ body, preparingAdditionalsForRendering }) => {
+    return (
+        <CardContent>
+            <Typography variant='h4' sx={{ backgroundColor: "honeydew", p: .2, mr: 6, ml: 15 }} dangerouslySetInnerHTML={{ __html: body }}></Typography>
+            <ShowUserPostMedias mediaContents={preparingAdditionalsForRendering} />
+        </CardContent>
+    )
+}
+
+const RenderPostCardHeader = ({ userData }) => {
+    let appCtx = useContext(AppContexts);
+
+    return (
+        <CardHeader
+            avatar={
+                <Link className='posted-by' style={{ textDecoration: "none" }} to={ appCtx.user._id ? `/users/${userData._id}/visit/profile` : '/'}>
+                    <Avatar
+                        src={userData?.ppUrl || "https://random.imagecdn.app/500/150"}
+                        sx={{ bgcolor: "red[500]", width: "74px", height: "74px" }}
+                        aria-label="recipe"
+                        alt={`User ${userData.fullName || ''} profile display`}
+                    />
+                    <p className='posted-by-tooltip-text'>{appCtx.user._id ? "Visit Profile" : "Login To Visit Profile"}</p>
+                </Link>
+            }
+            // action={
+            //     <IconButton aria-label="settings">
+            //         <MoreVertTwoTone />
+            //     </IconButton>
+            // }
+            title={
+                <Link className='posted-by' style={{ textDecoration: "none" }} to={ appCtx.user._id ? `/users/${userData._id}/visit/profile` : '/'}>
+                    <Typography variant="h4">{userData.fullName || "User Name"}</Typography>
+                    <p className='posted-by-tooltip-text'>{appCtx.user._id ? "Visit Profile" : "Login To Visit Profile"}</p>
+                </Link>
+            }
+            subheader={
+                <Typography variant="subtitle2">{`Member Since: ${moment(userData.created).fromNow()}`}</Typography>
+            }
+        />
     )
 }
 
